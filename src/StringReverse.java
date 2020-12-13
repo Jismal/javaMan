e864fadda082459b127d773348c1cb7c8386cbb5
JismalJoseph <jismaljoseph@yahoo.co.in>
2020-12-13 00:31:19 +0000

    string reverse

--- src/StringReverse.java
+++ src/StringReverse.java
@@ -1,17 +1,37 @@
 import java.io.StringBufferInputStream;
 
 public class StringReverse {
+    static String rev = "";
+
     //I want to reverse the strings
     public static void main(String[] args) {
+        StringReverse.reverseChar();
+        StringReverse.reverseStr();
         String actual = "this is a test string";
-        String rev = "";
+
         int len = actual.length();
         System.out.println(len);
+
         for (int i = len - 1; i >= 0; i--) {
             rev = rev + actual.charAt(i);
-
         }
         System.out.println(rev);
     }
-
+    // reverse using arrays
+    public static void reverseChar() {
+        String str = "I am going to reverse this string";
+        char[] charArr = str.toCharArray();
+        int lenA = charArr.length;
+        for (int i = lenA - 1; i >= 0; i--) {
+            rev = rev + charArr[i];
+        }
+        System.out.println(rev);
+    }
+    //using String buffer class
+    public static void reverseStr(){
+        String str2 = "I am going to reverse this string using Buffer";
+         StringBuffer sb= new StringBuffer(str2);
+         rev=sb.reverse().toString();
+        System.out.println(rev);
+    }
 }
